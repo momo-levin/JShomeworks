2d2f3e1e1d2b0cb166c2afe7b967c3c3c853fddc
Vikentsiy <vikentsiy05034@gmail.com>
2021-12-12 14:38:05 +0300

    added creation of an element by pressing a key

--- homework5/task3.js
+++ homework5/task3.js
@@ -27,9 +27,40 @@ function drawFigure(type, color, top, left, width, height) {
         div.style.height = height + "px";
         div.style.position = "absolute";
         document.body.appendChild(div);
-    } 
+    }
 }
-
+function addFigure(e) {
+    if (e.code == "KeyQ") {
+        let div = document.createElement('div');
+        div.style.background = 'red';
+        div.style.borderRadius = '50%';
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.position = "absolute";
+        div.style.top = '100px';
+        div.style.right = '100px';
+        document.body.appendChild(div);
+    } else if (e.code == 'KeyW') {
+        let div = document.createElement('div');
+        div.style.background = 'red';
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.position = "absolute";
+        div.style.top = '200px';
+        div.style.right = '200px';
+        document.body.appendChild(div);
+    } else if (e.code == "KeyE") {
+        let div = document.createElement('div');
+        div.style.background = 'red';
+        div.style.width = '50px';
+        div.style.height = '100px';
+        div.style.position = "absolute";
+        div.style.top = '300px';
+        div.style.right = '300px';
+        document.body.appendChild(div);
+    }
+}
+window.addEventListener('keydown', addFigure);
 drawFigure("круг", "red", 200, 200, 40);
 drawFigure("квадрат", "red", 100, 200, 40);
 drawFigure("прямоугольник", "red", 100, 500, 40, 200);
