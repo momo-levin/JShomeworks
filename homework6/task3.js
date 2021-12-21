ee6bd32fbb99990f31f794a7c1a9052d233f722e
momo-levin <86719961+momo-levin@users.noreply.github.com>
2021-12-21 17:17:05 +0300

    Update task3.js

--- homework6/task3.js
+++ homework6/task3.js
@@ -1,8 +1,9 @@
 "use strict"
-let checkbox1 = document.getElementById('checkbox1');
+let checkbox1 = document.getElementById('checkbox1');//не делай так больше
 const submitThirdForm = (e) => {
     let elemForm = e.target.elements;
     console.log(elemForm['input'].value)
+    //что за славный комментарий?
     /* if (elemForm['checkbox1'].checked = true) {
         console.log(elemForm['checkbox1'].name)
     } */
